9ef0cfff8b3c76ee9e0b9ffa43c289cf57befc16
Tim Lukashevich <silvernet@yandex.ru>
2024-12-26 21:35:12 +0300

    maximize on startup window decorations is "resize" cursor shape and blinking

--- config/appearance.lua
+++ config/appearance.lua
@@ -3,7 +3,7 @@ local gpu_adapters = require('utils.gpu-adapter')
 local colors = require('colors.custom')
 
 return {
-   max_fps = 120,
+   max_fps = 60,
    front_end = 'WebGpu',
    webgpu_power_preference = 'HighPerformance',
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
@@ -12,11 +12,13 @@ return {
    underline_thickness = '1.5pt',
 
    -- cursor
-   animation_fps = 120,
+   animation_fps = 60,
    cursor_blink_ease_in = 'EaseOut',
    cursor_blink_ease_out = 'EaseOut',
-   default_cursor_style = 'BlinkingBlock',
-   cursor_blink_rate = 650,
+   default_cursor_style = 'BlinkingBar', -- SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar,  BlinkingBar
+   cursor_blink_rate = 450,
+
+   window_decorations = 'RESIZE',
 
    -- color scheme
    colors = colors,
